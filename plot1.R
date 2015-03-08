4048c4bf5c6d5d377bc1c12d2ad68b3cc3bd672f
Naresh Raheja <nareshraheja@yahoo.com>
2015-03-08 15:42:33 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,5 +1,7 @@
-setwd("c:/EDA/proj1")
-getwd()
+## I used the next two lines to set working directory in my machine where the input data was stored
+## setwd("c:/EDA/proj1")
+## getwd()
+## the input data was stored in the working directory
 dataFile <- "./household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
@@ -9,4 +11,4 @@ globalActivePower <- as.numeric(subSetData$Global_active_power)
 png("plot1.png", width=480, height=480)
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
-
+## the plot has been stored as plot1.png
